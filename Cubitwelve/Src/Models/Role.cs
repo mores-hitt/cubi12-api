809768d70df58c28f2211f5ba2ce539c8990d097
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2023-10-28 11:17:38 -0300

    CUB-96 User and role

---
+++ Cubitwelve/Src/Models/Role.cs
@@ -0,0 +1,9 @@
+namespace Cubitwelve.Src.Models
+{
+    public class Role
+    {
+        public string Name { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+    }
+}
