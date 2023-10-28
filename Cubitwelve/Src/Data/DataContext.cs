905d999e027b41de9fabc3e2fcca82bcd282c0bb
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2023-10-28 11:25:35 -0300

    CUB-102 abstract and dbsets

--- Cubitwelve/Src/Data/DataContext.cs
+++ Cubitwelve/Src/Data/DataContext.cs
@@ -1,13 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Cubitwelve.Src.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Cubitwelve.Src.Data
 {
     public class DataContext : DbContext
     {
+
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Role> Roles { get; set; } = null!;
+        public DbSet<Subject> Subjects { get; set; } = null!;
+        public DbSet<SubjectRelationship> SubjectsRelationships { get; set; } = null!;
+        public DbSet<UserProgress> UsersProgress { get; set; } = null!;
+
         public DataContext(DbContextOptions options) : base(options)
         {
         }
