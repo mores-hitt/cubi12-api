ab03b5b7d259efa443ca8323a2b194b7c0cab52d
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2023-10-28 12:14:10 -0300

    CUB-116 Secret insecure

--- Cubitwelve/Src/Services/AuthService.cs
+++ Cubitwelve/Src/Services/AuthService.cs
@@ -1,6 +1,11 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Cubitwelve.Src.DTOs;
+using Cubitwelve.Src.Models;
 using Cubitwelve.Src.Repositories.Interfaces;
 using Cubitwelve.Src.Services.Interfaces;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Cubitwelve.Src.Services
 {
@@ -21,12 +26,39 @@ namespace Cubitwelve.Src.Services
 
         public async Task<string?> Login(LoginUserDto loginUserDto)
         {
-            return "jwt";
+            var user = await _usersRepository.GetByEmail(loginUserDto.Email);
+            if (user is null) return null;
+
+            var result = BCrypt.Net.BCrypt.Verify(loginUserDto.Password, user.HashedPassword);
+            if (!result) return null;
+
+            var token = CreateToken(user);
+            return token;
         }
 
         public async Task<string> RegisterClient(RegisterStudentDto registerClientDto)
         {
             return "jwt";
+        }
+
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>{
+                new ("email", user.Email)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                    _configuration.GetSection("AppSettings:Token").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(5),
+                signingCredentials: creds
+            );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
         }
     }
 }
