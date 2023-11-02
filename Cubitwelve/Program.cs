8223acfd323ddbaaa5cc74c9ec4625f34f8717eb
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2023-11-02 16:26:24 -0300

    CUB-148 Base Class and swagger refactored

--- Cubitwelve/Program.cs
+++ Cubitwelve/Program.cs
@@ -12,8 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddApplicationServices(builder.Configuration);
 // Load Database Connect Settings
 Env.Load();
 
--- Cubitwelve/Src/Extensions/AppServiceExtensions.cs
+++ Cubitwelve/Src/Extensions/AppServiceExtensions.cs
@@ -1,29 +1,15 @@
-using Cubitwelve.Src.Data;
-using Microsoft.EntityFrameworkCore;
-
 namespace Cubitwelve.Src.Extensions
 {
-    public static class AppSeedService
+    public static class AppServiceExtensions
     {
-        /// <summary>
-        /// Calls the seed method to populate the database with example data.
-        /// </summary>
-        /// <param name="app">Singleton WebApplication</param>
-        public static void SeedDatabase(WebApplication app)
+        public static void AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
-            var scope = app.Services.CreateScope();
-            var context = scope.ServiceProvider.GetRequiredService<DataContext>();
-            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-            try
-            {
-                // Migrate the database, create if it doesn't exist
-                context.Database.Migrate();
-                Seed.SeedData(context);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, " A problem ocurred during seeding ");
-            }
+            AddSwaggerGen(services);
+        }
+
+        private static void AddSwaggerGen(IServiceCollection services)
+        {
+            services.AddSwaggerGen();
         }
     }
 }
