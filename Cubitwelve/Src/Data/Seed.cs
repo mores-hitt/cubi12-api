06db7cf00aabbf12c51d06e688eab3254a22be9c
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2023-11-02 14:45:12 -0300

    CUB-146 Seed class and role methods

--- Cubitwelve/Src/Data/Seed.cs
+++ Cubitwelve/Src/Data/Seed.cs
@@ -1,7 +1,59 @@
+using System.Text.Json;
+using Cubitwelve.Src.Models;
+
 namespace Cubitwelve.Src.Data
 {
     public class Seed
     {
-        
+        /// <summary>
+        /// Seed the database with examples models in the json files if the database is empty.
+        /// </summary>
+        /// <param name="context">Database Context </param>
+        public static void SeedData(DataContext context)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            CallEachSeeder(context, options);
+        }
+
+        /// <summary>
+        /// Centralize the call to each seeder method and save changes.
+        /// </summary>
+        /// <param name="context">Database context</param>
+        /// <param name="options">Options to deserialize json</param>
+        public static void CallEachSeeder(DataContext context, JsonSerializerOptions options)
+        {
+            SeedFirstOrderTables(context, options);
+        }
+
+        /// <summary>
+        /// Seed the database with the tables that don't depend on other tables.
+        /// </summary>
+        /// <param name="context">Database context</param>
+        /// <param name="options">Options to deserialize json</param>
+        private static void SeedFirstOrderTables(DataContext context, JsonSerializerOptions options)
+        {
+            SeedRoles(context, options);
+            context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Seed the database with the roles in the json file if the database is empty.
+        /// </summary>
+        /// <param name="context">Database context</param>
+        /// <param name="options">Options to deserialize json</param>
+        private static void SeedRoles(DataContext context, JsonSerializerOptions options)
+        {
+            var result = context.Roles?.Any();
+            if (result is true or null) return;
+
+            var rolesData = File.ReadAllText("Data/Seeds/RolesData.json");
+            var rolesList = JsonSerializer.Deserialize<List<Role>>(rolesData, options) ??
+                throw new Exception("RolesData.json is empty");
+
+            context.Roles?.AddRange(rolesList); //Cannot be null because of the first if statement
+            context.SaveChanges();
+        }
+
     }
 }
