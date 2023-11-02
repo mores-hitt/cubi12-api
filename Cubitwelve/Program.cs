5c2677f575e1e9107c3768cbf5ec6ab6bdf9dd13
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2023-11-02 16:33:00 -0300

    CUB-148 DbContext DI refactored

--- Cubitwelve/Program.cs
+++ Cubitwelve/Program.cs
@@ -1,36 +1,12 @@
-using Cubitwelve.Src.Data;
-using Microsoft.EntityFrameworkCore;
-using DotNetEnv;
 using Cubitwelve.Src.Extensions;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddApplicationServices(builder.Configuration);
-// Load Database Connect Settings
-Env.Load();
-
-var user = Env.GetString("DB_USER");
-var password = Env.GetString("DB_PASSWORD");
-var database = Env.GetString("DB_DATABASE");
-var connectionString = $"server=localhost;user={user};password={password};database={database}";
-
-var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
-// Inject DbContext
-builder.Services.AddDbContext<DataContext>(opt =>
-                opt
-                .UseMySql(connectionString, serverVersion)
-                // FIXME: Remove the following 3 lines in production
-                .LogTo(Console.WriteLine, LogLevel.Information)
-                .EnableSensitiveDataLogging()
-                .EnableDetailedErrors()
-);
 
 var app = builder.Build();
 
--- Cubitwelve/Src/Extensions/AppServiceExtensions.cs
+++ Cubitwelve/Src/Extensions/AppServiceExtensions.cs
@@ -1,15 +1,47 @@
+using Cubitwelve.Src.Data;
+using Microsoft.EntityFrameworkCore;
+using DotNetEnv;
+
 namespace Cubitwelve.Src.Extensions
 {
     public static class AppServiceExtensions
     {
         public static void AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
+            InitEnvironmentVariables();
             AddSwaggerGen(services);
+            AddDbContext(services);
+        }
+
+        private static void InitEnvironmentVariables()
+        {
+            DotNetEnv.Env.Load();
         }
 
         private static void AddSwaggerGen(IServiceCollection services)
         {
             services.AddSwaggerGen();
         }
+
+        private static void AddDbContext(IServiceCollection services)
+        {
+            var user = Env.GetString("DB_USER");
+            var password = Env.GetString("DB_PASSWORD");
+            var database = Env.GetString("DB_DATABASE");
+            var connectionString = $"server=localhost;user={user};password={password};database={database}";
+
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
+            // Inject DbContext
+            services.AddDbContext<DataContext>(opt =>
+                            opt
+                            .UseMySql(connectionString, serverVersion)
+                            // TODO: Remove the following 3 lines in production
+                            .LogTo(Console.WriteLine, LogLevel.Information)
+                            .EnableSensitiveDataLogging()
+                            .EnableDetailedErrors()
+            );
+        }
+
+
     }
 }
