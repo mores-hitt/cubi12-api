18288c173294caeb5fdd8a7f083f980bc61defc8
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2023-11-03 02:34:25 -0300

    CUB-152

--- Cubitwelve/Src/Services/Interfaces/IUsersService.cs
+++ Cubitwelve/Src/Services/Interfaces/IUsersService.cs
@@ -1,7 +1,13 @@
+using Cubitwelve.Src.DTOs.Models;
+
 namespace Cubitwelve.Src.Services.Interfaces
 {
     public interface IUsersService
     {
-        
+        public Task<List<UserDto>> GetAll();
+
+        public Task<UserDto> GetById(int id);
+
+        public Task<UserDto> GetByEmail(string email);
     }
 }
--- Cubitwelve/Src/Services/UsersService.cs
+++ Cubitwelve/Src/Services/UsersService.cs
@@ -1,9 +1,39 @@
+using Cubitwelve.Src.DTOs.Models;
+using Cubitwelve.Src.Models;
+using Cubitwelve.Src.Repositories.Interfaces;
 using Cubitwelve.Src.Services.Interfaces;
 
 namespace Cubitwelve.Src.Services
 {
     public class UsersService : IUsersService
     {
-        
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapperService _mapperService;
+
+        public UsersService(IUnitOfWork unitOfWork, IMapperService mapperService)
+        {
+            _unitOfWork = unitOfWork;
+            _mapperService = mapperService;
+        }
+
+        public async Task<List<UserDto>> GetAll()
+        {
+            var users = await _unitOfWork.UsersRepository.GetAll();
+            return _mapperService.MapList<User, UserDto>(users);
+        }
+
+        public async Task<UserDto> GetByEmail(string email)
+        {
+            var user = await _unitOfWork.UsersRepository.GetByEmail(email)
+                                        ?? throw new Exception("User not found");
+            return _mapperService.Map<User, UserDto>(user);
+        }
+
+        public async Task<UserDto> GetById(int id)
+        {
+            var user = await _unitOfWork.UsersRepository.GetByID(id)
+                                        ?? throw new Exception("User not found");
+            return _mapperService.Map<User, UserDto>(user);
+        }
     }
 }
