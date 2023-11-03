47e2b97c7ee8e7e6d3478765eebc4d7cf60aeae9
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2023-11-03 18:00:41 -0300

    CUB-176

--- Cubitwelve/Src/Common/Constants/RegularExpressions.cs
+++ Cubitwelve/Src/Common/Constants/RegularExpressions.cs
@@ -1,10 +1,15 @@
+using System.Text.RegularExpressions;
+
 namespace Cubitwelve.Src.Common.Constants
 {
-    public static class RegularExpressions
+    public static partial class RegularExpressions
     {
         /// <summary>
         /// Regex for password validation, must have at least one letter and one number
         /// </summary>
         public const string PasswordValidation = @"^(?=.*[a-zA-Z])(?=.*\d).+$";
+
+        [GeneratedRegex("^([0-9]+-[0-9K])$", RegexOptions.Compiled)]
+        public static partial Regex RutRegex();
     }
 }
--- Cubitwelve/Src/DataAnnotations/RutAttribute.cs
+++ Cubitwelve/Src/DataAnnotations/RutAttribute.cs
@@ -1,34 +1,33 @@
 using System.ComponentModel.DataAnnotations;
-using System.Text.RegularExpressions;
+using Cubitwelve.Src.Common.Constants;
 
 namespace Cubitwelve.Src.DataAnnotations
 {
+    // https://gist.github.com/donpandix/045f865c3bf800893036
     public class RutAttribute : ValidationAttribute
     {
         public override bool IsValid(object? value)
         {
             var rut = value?.ToString() ?? string.Empty;
             if (rut.Length != 12) return false;
-            return ValidaRut(rut);
+            return CheckRut(rut);
         }
 
-        public static bool ValidaRut(string rut)
+        public static bool CheckRut(string rut)
         {
             rut = rut.Replace(".", "").ToUpper();
-            Regex expresion = new("^([0-9]+-[0-9K])$");
-            string dv = rut.Substring(rut.Length - 1, 1);
-            if (!expresion.IsMatch(rut))
-            {
+            if (!RegularExpressions.RutRegex().IsMatch(rut))
                 return false;
-            }
+
+            string dv = rut.Substring(rut.Length - 1, 1);
             char[] dash = { '-' };
             string[] splittedRut = rut.Split(dash);
-            if (dv != Digito(int.Parse(splittedRut[0])))
+            if (dv != CalculateDigit(int.Parse(splittedRut[0])))
                 return false;
             return true;
         }
 
-        public static string Digito(int rut)
+        public static string CalculateDigit(int rut)
         {
             int sum = 0;
             int multiplier = 1;
