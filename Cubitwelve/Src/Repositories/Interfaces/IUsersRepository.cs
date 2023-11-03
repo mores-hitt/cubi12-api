826e44d4cddae9236520e6dfa8126dc63a4ec4b7
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2023-11-03 01:48:51 -0300

    CUB-151

--- Cubitwelve/Src/Repositories/Interfaces/IUsersRepository.cs
+++ Cubitwelve/Src/Repositories/Interfaces/IUsersRepository.cs
@@ -4,6 +4,19 @@ namespace Cubitwelve.Src.Repositories.Interfaces
 {
     public interface IUsersRepository : IGenericRepository<User>
     {
-        
+
+
+        /// <summary>
+        /// Get all users
+        /// </summary>
+        /// <returns>List of users</returns>
+        public Task<List<User>> GetAll();
+
+        /// <summary>
+        /// Get user by email
+        /// </summary>
+        /// <param name="email">The email of the user</param>
+        /// <returns>User or null</returns>
+        public Task<User?> GetByEmail(string email);
     }
 }
--- Cubitwelve/Src/Repositories/UsersRepository.cs
+++ Cubitwelve/Src/Repositories/UsersRepository.cs
@@ -1,13 +1,35 @@
+using System.Linq.Expressions;
 using Cubitwelve.Src.Data;
 using Cubitwelve.Src.Models;
 using Cubitwelve.Src.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cubitwelve.Src.Repositories
 {
     public class UsersRepository : GenericRepository<User>, IUsersRepository
     {
-        public UsersRepository(DataContext context) : base(context)
+        private readonly Expression<Func<User, bool>> softDeleteFilter = x => x.DeletedAt == null;
+
+        public UsersRepository(DataContext context) : base(context) { }
+
+        public async Task<List<User>> GetAll()
         {
+            var users = await dbSet
+                            .Where(softDeleteFilter)
+                            .Include(x => x.Role)
+                            .Include(x => x.Career)
+                            .ToListAsync();
+            return users;
+        }
+
+        public Task<User?> GetByEmail(string email)
+        {
+            var user = dbSet
+                        .Where(softDeleteFilter)
+                        .Include(x => x.Role)
+                        .Include(x => x.Career)
+                        .FirstOrDefaultAsync(x => x.Email == email);
+            return user;
         }
     }
 }
