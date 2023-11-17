1fcec34e721941416b3ed19d2eec28e9baaf009d
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2023-11-17 03:49:31 -0300

    CUB-222

--- Cubitwelve/Src/Data/Seed.cs
+++ Cubitwelve/Src/Data/Seed.cs
@@ -24,6 +24,7 @@ namespace Cubitwelve.Src.Data
         public static void CallEachSeeder(DataContext context, JsonSerializerOptions options)
         {
             SeedFirstOrderTables(context, options);
+            SeedSecondtOrderTables(context, options);
         }
 
         /// <summary>
@@ -36,6 +37,16 @@ namespace Cubitwelve.Src.Data
             SeedRoles(context, options);
             SeedSubjects(context, options);
             SeedCareers(context, options);
+        }
+
+        /// <summary>
+        /// Seed the database with the tables whose data depends on exatly one table.
+        /// </summary>
+        /// <param name="context">Database context</param>
+        /// <param name="options">Options to deserialize json</param>
+        private static void SeedSecondtOrderTables(DataContext context, JsonSerializerOptions options)
+        {
+            SeedSubjectsRelationships(context, options);
         }
 
         /// <summary>
@@ -71,9 +82,10 @@ namespace Cubitwelve.Src.Data
             var subjectsData = File.ReadAllText(path);
             var subjectsList = JsonSerializer.Deserialize<List<Subject>>(subjectsData, options) ??
                 throw new Exception("SubjectsData.json is empty");
-            // Normalize the name and department of the subjects
+            // Normalize the name, code and department of the subjects
             subjectsList.ForEach(s =>
             {
+                s.Code = s.Code.ToLower();
                 s.Name = s.Name.ToLower();
                 s.Department = s.Department.ToLower();
             });
@@ -95,13 +107,38 @@ namespace Cubitwelve.Src.Data
             var careersData = File.ReadAllText(path);
             var careersList = JsonSerializer.Deserialize<List<Career>>(careersData, options) ??
                 throw new Exception("CareersData.json is empty");
-            // Normalize the name of the careers
+            // Normalize the name and code of the careers
             careersList.ForEach(s =>
             {
                 s.Name = s.Name.ToLower();
             });
 
             context.Careers?.AddRange(careersList);
+            context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Seed the database with the subjects relationships in the json file and save changes if the database is empty.
+        /// </summary>
+        /// <param name="context">Database context</param>
+        /// <param name="options">Options to deserialize json</param>
+        private static void SeedSubjectsRelationships(DataContext context, JsonSerializerOptions options)
+        {
+            var result = context.SubjectsRelationships?.Any();
+            if (result is true or null) return;
+            var path = "Src/Data/DataSeeders/SubjectsRelationsData.json";
+            var subjectsRelationshipsData = File.ReadAllText(path);
+            var subjectsRelationshipsList = JsonSerializer
+                .Deserialize<List<SubjectRelationship>>(subjectsRelationshipsData, options) ??
+                throw new Exception("SubjectsRelationsData.json is empty");
+            // Normalize the codes of the codes
+            subjectsRelationshipsList.ForEach(s =>
+            {
+                s.SubjectCode = s.SubjectCode.ToLower();
+                s.PreSubjectCode = s.PreSubjectCode.ToLower();
+            });
+
+            context.SubjectsRelationships?.AddRange(subjectsRelationshipsList);
             context.SaveChanges();
         }
 
