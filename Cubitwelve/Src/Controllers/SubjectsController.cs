b8121fea61a93baff25597df131ec444d0cc94ef
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2023-11-17 23:11:42 -0300

    CUB-237

--- Cubitwelve/Src/Controllers/SubjectsController.cs
+++ Cubitwelve/Src/Controllers/SubjectsController.cs
@@ -20,11 +20,25 @@ namespace Cubitwelve.Src.Controllers
             return Ok(subjects);
         }
 
-        [HttpGet("relationships")]
-        public async Task<ActionResult<List<SubjectRelationshipDto>>> GetAllRelationships()
+        [HttpGet("prerequisites-map/objects")]
+        public async Task<ActionResult<List<SubjectRelationshipDto>>> GetAllPreRequisitesMapObjects()
         {
             var relationships = await _subjectsService.GetAllRelationships();
             return Ok(relationships);
+        }
+
+        [HttpGet("prerequisites-map")]
+        public async Task<ActionResult<Dictionary<string, List<string>>>> GetAllPreRequisitesMap()
+        {
+            var preRequisitesMap = await _subjectsService.GetPreRequisitesMap();
+            return Ok(preRequisitesMap);
+        }
+
+        [HttpGet("postrequisites-map")]
+        public async Task<ActionResult<Dictionary<string, List<string>>>> GetAllPostRequisitesMap()
+        {
+            var postRequisitesMap = await _subjectsService.GetPostRequisitesMap();
+            return Ok(postRequisitesMap);
         }
     }
 }
