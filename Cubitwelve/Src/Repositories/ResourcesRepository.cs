14e6e8d26b844a39e2b322cbe3760050d48636fc
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2023-12-08 01:29:57 -0300

    feat: Base Repository

---
+++ Cubitwelve/Src/Repositories/ResourcesRepository.cs
@@ -0,0 +1,12 @@
+﻿using Cubitwelve.Src.Data;
+using Cubitwelve.Src.Models;
+using Cubitwelve.Src.Repositories.Interfaces;
+
+namespace Cubitwelve.Src.Repositories;
+
+public class ResourcesRepository : GenericRepository<SubjectResource>, IResourcesRepository
+{
+    public ResourcesRepository(DataContext context) : base(context)
+    {
+    }
+}
