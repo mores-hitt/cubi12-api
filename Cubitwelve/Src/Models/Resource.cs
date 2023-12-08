b020bf499fa3311e4d3c8530587c25f4066e32d7
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2023-12-08 01:19:31 -0300

    feat: Entities added

---
+++ Cubitwelve/Src/Models/Resource.cs
@@ -0,0 +1,14 @@
+﻿namespace Cubitwelve.Src.Models;
+
+public class Resource : BaseModel
+{
+    public string Type { get; set; } = null!;
+
+    public int TypeCode { get; set; }
+
+    public string Url { get; set; } = null!;
+
+    public SubjectResource SubjectResource { get; set; } = null!;
+
+    public int SubjectResourceId { get; set; }
+}
